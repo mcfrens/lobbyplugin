4744b46b4dd4a7a15bcf307548dd43bed09cdacf
Brendan Perry <brendan@brendanperry.me>
2023-09-11 23:33:27 -0400

    adding files

---
+++ src/main/java/com/brendanperry/LobbyPlugin.java
@@ -0,0 +1,29 @@
+package com.brendanperry;
+
+import com.brendanperry.commands.SendToServer;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.messaging.PluginMessageListener;
+import org.jetbrains.annotations.NotNull;
+
+public final class LobbyPlugin extends JavaPlugin implements @NotNull PluginMessageListener {
+
+    @Override
+    public void onEnable() {
+        this.getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
+        this.getServer().getMessenger().registerIncomingPluginChannel(this, "BungeeCord", this);
+
+        this.getCommand("teleporttoserver").setExecutor(new SendToServer(this));
+    }
+
+    @Override
+    public void onDisable() {
+        this.getServer().getMessenger().unregisterOutgoingPluginChannel(this);
+        this.getServer().getMessenger().unregisterIncomingPluginChannel(this);
+    }
+
+    @Override
+    public void onPluginMessageReceived(@NotNull String channel, @NotNull Player player, @NotNull byte[] message) {
+
+    }
+}
---
+++ src/main/java/com/brendanperry/commands/SendToServer.java
@@ -0,0 +1,55 @@
+package com.brendanperry.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+public class SendToServer implements CommandExecutor {
+    private final Plugin plugin;
+
+    public SendToServer(Plugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        String serverName = args[0];
+
+        if (args.length != 1) {
+            return false;
+        }
+
+        List<Entity> entities = Bukkit.selectEntities(sender, "@p");
+
+        for (Entity entity : entities) {
+            if (entity instanceof Player player) {
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                DataOutputStream outputStream = new DataOutputStream(byteArrayOutputStream);
+
+                try {
+                    outputStream.writeUTF("ConnectOther");
+                    outputStream.writeUTF(player.getName());
+                    outputStream.writeUTF(serverName);
+                } catch (IOException e) {
+                    return false;
+                }
+
+                Bukkit.getServer().sendPluginMessage(plugin, "BungeeCord", byteArrayOutputStream.toByteArray());
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
